cbdb0a5f71530e5af0df85643535973118e5f0b0
ngoc233 <ngoc2331995@gmail.com>
2017-05-29 10:39:25 +0700

    change 5/29/2017

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -15,20 +15,7 @@ $(document).ready(function(){
 	/*	Menu item highlighting
 	/* ========================================================================= */
 
-	jQuery('#nav').singlePageNav({
-		offset: jQuery('#nav').outerHeight(),
-		filter: ':not(.external)',
-		speed: 1200,
-		currentClass: 'current',
-		easing: 'easeInOutExpo',
-		updateHash: true,
-		beforeStart: function() {
-			console.log('begin scrolling');
-		},
-		onComplete: function() {
-			console.log('done scrolling');
-		}
-	});
+	
 	
     $(window).scroll(function () {
         if ($(window).scrollTop() > 400) {
@@ -146,6 +133,7 @@ function initialize() {
     });
 
 }
+
 
 google.maps.event.addDomListener(window, "load", initialize);
 // ========== END GOOGLE MAP ========== //
